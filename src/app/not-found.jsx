a9b084180ab90587a8f5724dfec718f8ce08fa86
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-15 14:29:20 +0600

    Not Found Page is added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+import NotFoundImage from "@/Assets/not-found-image.png";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { robotoslab } from "./layout";
+import Link from "next/link";
+const notFoundPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row justify-center gap-10 items-center h-screen">
+      <div>
+        <Image
+          src={NotFoundImage}
+          alt="Not-Found-Image"
+          height={280}
+          width={280}
+          className="mx-auto"
+        />
+      </div>
+      <div className="space-y-3 h-50 text-center md:text-left">
+        <h3 className="{robotoslab} font-bold text-3xl">Oops!</h3>
+        <p>We could not find the page you were looking for</p>
+        <span className="flex justify-center md:justify-start">
+          {" "}
+          <Link href={"/"}>
+            {" "}
+            <button className="flex btn rounded-full btn-neutral">
+              <FaArrowLeftLong />
+              Go Home
+            </button>
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default notFoundPage;
