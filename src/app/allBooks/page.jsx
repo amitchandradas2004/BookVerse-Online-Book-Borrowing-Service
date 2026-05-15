9ac0308cff0b3e60382b2d22aded4433920d2a23
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-16 00:17:06 +0600

    All Books page is added

--- src/app/allBooks/page.jsx
+++ src/app/allBooks/page.jsx
@@ -1,8 +1,20 @@
- 
-const AllBooksPage = () => {
+import Book from "@/components/HomePage/Book";
+
+const AllBooksPage = async () => {
+  const res = await fetch(
+    `https://online-book-borrowing-platform.vercel.app/Books.json`,
+  );
+  const data = await res.json();
+  const allBooks = data.books;
+  // console.log(allBooks);
+
   return (
-    <div className="mt-20">
-      This is all books page
+    <div className="mt-30 container mx-auto px-2 sm:px-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+        {allBooks.map((book) => (
+          <Book key={book.id} book={book}></Book>
+        ))}
+      </div>
     </div>
   );
 };
